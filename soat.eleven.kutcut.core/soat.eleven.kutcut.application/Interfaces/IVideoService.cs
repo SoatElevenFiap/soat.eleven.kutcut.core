ee1f165721b89d1be7094ac43d6347c422e44bdf
Adriano Ricardo Felippe Torini <adrianotorini@hotmail.com>
2026-02-10 15:22:10 -0300

    Adiciona método para atualizar status do video, e método em infra para enviar mensagens a uma fila especifica.

--- soat.eleven.kutcut.core/soat.eleven.kutcut.application/Interfaces/IVideoService.cs
+++ soat.eleven.kutcut.core/soat.eleven.kutcut.application/Interfaces/IVideoService.cs
@@ -12,6 +12,7 @@ namespace soat.eleven.kutcut.application.Interfaces
         Task<Result<VideoResult>> GetByIdAsync(Guid id);
         Task<Result<PagedResult<VideoResult>>> GetByStatusAsync(StatusEnum status, int pageNumber, int pageSize);
         Task<Result<VideoResult>> UpdateTitleAsync(Guid id, string? newTitle);
+        Task<Result<VideoResult>> UpdateStatusAsync(Guid id, StatusEnum newStatus);
         Task<Result<Stream>> DownloadThumbnailsAsync(Guid id);
     }
 }
